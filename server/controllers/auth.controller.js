06f7874cbf050f68b5e3ae2219430a708e14d1d7
Tushar Singh <tusharsinghkrj@gmail.com>
2026-02-22 01:17:43 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -9,14 +9,13 @@ export const googleAuth=async(req,res)=>{
       user=await User.create({name,email})
     }
     const token=await getToken(user._id)
-    res.cookie("token",token,{
-      httpOnly:true,
-      secure:true,
-      sameSite:"none",
-      path:"/"
-      maxAge:7*24*60*60*1000
-       
-    })
+   res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  path: "/",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
     return res.status(200).json({message:"Authentication successful",user})
   }catch(err){
     return res.status(500).json({message:`googleSignUp Erroe {err}`})
